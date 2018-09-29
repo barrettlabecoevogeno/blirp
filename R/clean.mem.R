273dc456a79873d09ff39f67fe82095d09cd20af
Marc-Olivier Beausoleil <beausoleilmo@gmail.com>
2018-09-29 09:42:29 -0400

    Made my first help page for this function

--- R/clean.mem.R
+++ R/clean.mem.R
@@ -1,1 +1,17 @@
+#' Remove everything from R memory
+#'
+#' @description Will delete everything in R's memory
+#' @param ... Runs by itself.
+#' @return Deletes everything
+#' @note Just be careful before deleting your Global environment to be sure this is what you wanted!
+#'
+#' @export
+#'
+#' @examples
+#' # With vector
+#' z <- c(1,2,3, NA)
+#' rm(list=ls(all=TRUE))
+#' z
+#'
+
 rm(list=ls(all=TRUE))
